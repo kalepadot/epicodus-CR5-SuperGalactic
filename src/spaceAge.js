9049182a97b8e740fefe1d4cc1246d3bfd607333
Jeremy Kale Padot <jeremy@padot.us>
2020-02-07 13:40:29 -0800

    adjust js spacing/indenting

--- src/spaceAge.js
+++ src/spaceAge.js
@@ -6,24 +6,28 @@ export class UserAge {
   this.planet = planet;
 }
 
-mercury() {
-  this.planet = .24;
-  const mercuryAge = (this.age / this.planet);
-  return mercuryAge;
-}
-venus() {
-  this.planet = .62;
-  const venusAge =  Math.round(this.age / this.planet);
-  return venusAge;
-}
-mars() {
-  this.planet = 1.88;
-  const marsAge = Math.round(this.age / this.planet);
-  return marsAge;
+  mercury() {
+    this.planet = .24;
+    const mercuryAge = (this.age / this.planet);
+    return mercuryAge;
   }
+
+  venus() {
+    this.planet = .62;
+    const venusAge =  Math.round(this.age / this.planet);
+    return venusAge;
+  }
+
+  mars() {
+    this.planet = 1.88;
+    const marsAge = Math.round(this.age / this.planet);
+    return marsAge;
+  }
+
   jupiter() {
-    this.planet = 11.86;
-    const jupiterAge = Math.round(this.age / this.planet);
-    return jupiterAge;
+      this.planet = 11.86;
+      const jupiterAge = Math.round(this.age / this.planet);
+      return jupiterAge;
   }
+
 }
