e813e761597e7a5b7d5d339e0a17ee10af084c23
Jeremy Kale Padot <jeremy@padot.us>
2020-02-07 09:58:42 -0800

    test recognition of UserAge class constructor

---
+++ __tests__/spaceAge.tests.js
@@ -0,0 +1,8 @@
+import { UserAge } from "../src/spaceAge";
+
+describe("UserAge", () => {
+test("should reconize UserAge", () => {
+  // let userAge = new UserAge();
+  expect(UserAge).toEqual(UserAge);
+});
+});
